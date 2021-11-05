456621311bc5261bd38c7ebdf2f48ff0bc183195
DanielB <d.banasiewicz@pdo.com.pl>
2021-11-05 07:59:02 +0100

    change method for fetch data from API; styles for users list added; change fetch result from 5 to 1;

--- src/components/ButtonFetchUsers.js
+++ src/components/ButtonFetchUsers.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 const ButtonFetchUsers = props => {
     return (
-        <button onClick={props.click}>Dodaj 5 użytkowników</button>
+        <button
+            style={{
+                padding: '10px 20px',
+                border: '2px solid black',
+                backgroundColor: 'white',
+                margin: '20px'
+            }}
+            onClick={props.click}>Dodaj użytkownika
+        </button>
 
     );
 }
